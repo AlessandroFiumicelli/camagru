a795e163b19f4685c5e6d904eb59da1a40f3e435
Rivers <umberto.carletti@gmail.com>
2020-02-03 19:13:34 +0100

    add first shape of php

---
+++ db/setup.php
@@ -0,0 +1,60 @@
+<?php
+$pdo = new PDO($DB_DNS_SETUP, $DB_USER, $DB_PASSWORD);
+$pdo->query("DROP DATABASE IF EXISTS camagru");
+$pdo->query("CREATE DATABASE IF NOT EXISTS camagru");
+$pdo = new PDO ($DB_DSN, $DB_USER, $DB_PASSWORD);
+$sql_us = "CREATE TABLE `users` (
+			`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			`username` VARCHAR(11) DEFAULT "Jhon Doe",
+			`email` VARCHAR(50) NOT NULL,
+			`password` VARCHAR(255) NOT NULL,
+			`name` VARCHAR(11) NOT NULL,
+			`surname` VARCHAR(11) NOT NULL,
+			`birthday` DATE,
+			`reset_password` VARCHAR(1) NOT NULL, DEFAULT 'N',
+			`verified` VARCHAR(1) NOT NULL, DEFAULT 'N',
+			`img_profile` VARCHAR(100) NOT NULL,
+			`creation_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+			)";
+
+$sql_img= "CREATE TABLE `images` (
+			`id` INT NOT NUL AUTO_INCREMENT PRIMARY KEY,
+			`user_id` INT(11) NOT NULL,
+			`img` VARCHAR(100) NOT NULL,
+			`creation_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+			FOREING KEY (user_id) REFERENCES users(id)
+			)";
+
+$sql_likes = "CREATE TABLE `likes` (
+			`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			`user_id` INT(11) NOT NULL,
+			`img_id` INT(11) NOT NULL,
+			FOREING KEY (user_id) REFERENCES users(id),
+			FOREING KEY (img_id) REFERENCES images(id)
+			)";
+
+$sql_follow = "CREATE TABLE `follow` (
+			`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			`id_followed` INT(11) NOT NULL,
+			`id_follower` INT(11) NOT NULL,
+			FOREING KEY (id_followed) REFERENCES users(id),
+			FOREING KEY (id_follower) REFERENCES users(id)
+			)";
+
+$sql_cmts = "CREATE TABLE `comments` (
+			`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			`user_id` INT(11) NOT NULL,
+			`img_id` INT(11) NOT NULL,
+			`text` VARCHAR(255) NOT NULL,
+			`creation_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+			FOREIGN KEY (user_id) REFERENCES user(id),
+			FOREING KEY (img_id) REFERENCES images(id)
+			)";
+
+$pdo->exec($sql_us);
+$pdo->exec($sql_img);
+$pdo->exec($sql_likes);
+$pdo->exec($sql_follow);
+$pdo->exec($sql_cmts);
+file_put_contents("installed", "1");
+?>
---
+++ subscribe.php
@@ -0,0 +1,147 @@
+<?php
+session_start();
+include 'db/database.php'
+include 'mail.php';
+
+if (isset($_COOKIE['logged_in']) && !empty($_COOKIE['logged_in']))
+?>
+<html>
+<head>
+	<title>Camagru</title>
+	<style>
+		body {
+			background-color: rgb(38, 38, 38);
+			font: 95% Arial, Helvetica, sans-serif;
+			}
+		#content {
+			width: 560px;
+			height: 864px;
+			margin: 86px 680px 0px 680px;
+			display: block;
+			float: left;
+		}
+		
+		#sub {
+			width: 560px;
+			float: left;
+			display: block;
+			margin: 5px auto;
+			padding: 5px;
+			background-color: #FFFAF0;
+			border: 0.1px solid;
+			border-color: rgb(38, 38, 38);
+			border-radius: 3px;
+			opacity: 0.9;
+		}
+		#sub input[type="email"],
+		#sub input[type="text"],
+		#sub input[type="password"]
+		{
+			-webkit-transition: all 0.30s ease-in-out;
+			-moz-transition: all 0.30s ease-in-out;
+			-ms-transition: all 0.30s ease-in-out;
+			-o-transition: all 0.30s ease-in-out;
+			outline: none;
+			box-sizing: border-box;
+			-webkit-box-sizing: border-box;
+			-moz-box-sizing: border-box;
+			width: 50%;
+			background: #fff;
+			margin-bottom: 4%;
+			border: 1px solid #ccc;
+			padding: 3%;
+			border-radius: 3px;
+			color: #555;
+			font: 95% Arial, Helvetica, sans-serif;
+		}
+
+
+		#sub input[type="email"]:focus,
+		#sub input[type="text"]:focus,
+		#sub input[type="password"]:focus {
+			box-shadow: 0 0 5px #43D1AF;
+			padding: 3%;
+			border: 1px solid #43D1AF;
+			border-radius: 3px;
+		}
+
+		#sub input[type="submit"],
+		#sub input[type="botton"] 
+		{
+			box-sizing: border-box;
+			-webkit-box-sizing: border-box;
+			-moz-box-sizing: border-box;
+			width: 50%;
+			padding: 3%;
+			background: #2471FF;
+			border-radius: 3px;
+			border-bottom: 3px solid #5994FF;
+			border-top-style: none;
+			border-right-style: none;
+			border-left-style: none;
+			color: #D2E2FF;
+		}
+
+		#sub input[type="submit"]:hover,
+		#sub input[type="botton"]:hover {
+			background: #6B9FFF;
+			color:#fff;
+		}
+		#login {
+			width: 560px;
+			height: 50px;
+			border-radius: 3px;
+			margin: 5px auto;
+                        padding: 5px;
+                        background-color: #FFFAF0;
+                        border: 0.1px solid;
+                        border-color: rgb(38, 38, 38);
+                        opacity: 0.9;
+			float: left;
+		}
+		#login p {
+			text-align: center;
+			}
+
+		.footer {
+			width: 1152px;
+			height: 50px;
+			float: left;
+			margin: 0px 384px 0px 384px;
+			}
+		.footer p {
+			color: #DCDCDC;
+			padding-bottom: 5px;
+		}
+		.logo {
+			margin: 5% auto;
+			width: 30%;
+		}	
+	</style>
+</head>
+<body>
+	<div id="content">
+		<div id="sub">
+		<center>
+			<img class="logo" src="./img/Camagru_logo_b.png" alt="Camagru">
+			<p>Subscribe to see your friends photo.</p>
+			<form action='./register.php' method='POST'>
+				</br><input placeholder="Email*"  required="true" name="email" type="email" value="" />
+				</br><input placeholder="Name and Surname*" required="true" name="name" type="text" value=""/>
+				</br><input placeholder="Username*" required="true" name="username" type="text" value=""/>
+				</br><input placeholder="Password*" required="true" name="password" type="password" value=""/>
+				</br><input placeholder="Repeat Password*" required="true" name="rpassword" type="password" value=""/>
+				</br><input type="submit" name="singup" value="Subscribe"></input> 
+			</form>
+		</center>
+		</div>
+		<div id="login">
+			<p>Do you have an account? <a href="./login.html">Log in</a></p>
+		</div>
+	</div>
+	<div class="footer">
+		<p>&copy; alfiumic 2020</p>
+	</div>
+
+</body>
+</html>
