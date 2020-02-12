990b5b23613baddfcb30fb35b5a4aa51cddc9755
Rivers <umberto.carletti@gmail.com>
2020-02-12 16:39:55 +0100

    fix quyery-

--- config/database.php
+++ config/database.php
@@ -1,14 +1,14 @@
 <?php
-$DB_HOST = 'localhost:8080';
+$DB_HOST = 'localhost:3306';
 $DB_CREATOR = 'creator';
-$DB_CREATOR_PSW = '12345678';
+$DB_CREATOR_PSW = '@Creator1';
 $DB_USER = 'alfiumic';
 $DB_NAME = 'camagru';
-$DB_PASSWORD = 'alfiumic';
+$DB_PASSWORD = '@Alfiumic1';
 $DB_DSN_SETUP = 'mysql:host='.$DB_HOST.';';
 $DB_DSN = 'mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';';
 $check = file_get_contents("installed");
-if ($check != '1')
-	include 'config/newuser.php';
+if ($check != '1'){
 	include 'config/setup.php';
+}
 ?>
--- config/newuser.php
+++ config/newuser.php
@@ -1,11 +1,11 @@
 <?php
-$pdo = new PDO($DB_DNS_SETUP, 'root', '01234567');
+$pdo = new PDO($DB_DSN_SETUP, 'root', '01234567');
 $sql_alfiumic = "CREATE USER 'alfiumic'@'%' IDENTIFIED BY 'alfiumic'";
 $sql_creator = "CREATE USER 'creator'@'%' IDENTIFIED BY '12345678'";
 $sql_alfiumic_grant = "GRANT insert, select, update, delete, file ON *.* TO 'alfiumic'@'%'";
 $sql_creator_grant = "GRANT insert, select, update, delete, file, create, drop ON *.* TO 'creator'@'%'";
-pdo->exec($sql_alfiumic);
-pdo->exec($sql_alfiumic_grant);
-pdo->exec($sql_creator);
-pdo->exec($sql_creator_grant);
+$pdo->exec($sql_alfiumic);
+$pdo->exec($sql_alfiumic_grant);
+$pdo->exec($sql_creator);
+$pdo->exec($sql_creator_grant);
 ?>
